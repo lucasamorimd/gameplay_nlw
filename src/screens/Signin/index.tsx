a83dec6e565d428e8311dc540e3e63f273555018
Lucas Amorim <lucas.ad@hotmail.com>
2021-06-23 22:16:46 -0300

    continuação das aulas

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import {
     View,
     Text,
-    Image,
-    StatusBar
+    Image
 } from 'react-native';
 
 import { ButtonIcon } from '../../components/buttonicon';
 import IllustratorImg from '../../assets/illustration.png';
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 
 
 export function Signin() {
-
+    const navigation = useNavigation()
+    function handleSignIn() {
+        navigation.navigate('Home')
+    }
     return (
         <View style={styles.container}>
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
+
             <Image
                 source={IllustratorImg}
                 style={styles.image}
@@ -28,18 +27,18 @@ export function Signin() {
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas{`\n`}
-                    facilmente
+                    Conecte-se {'\n'}
+                    e organize suas{'\n'}
+                    jogatinas
                 </Text>
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar seus games{`\n`}
                     favoritos com seus amigos
                 </Text>
                 <ButtonIcon
-                 title="Entrar com Discord"
-                 activeOpacity={0.7}
-                 />
+                    title="Entrar com Discord"
+                    onPress={handleSignIn}
+                />
             </View>
         </View>
     );
